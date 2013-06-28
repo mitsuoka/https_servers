c0a6fe9471fb195204c6b94a6f91585a74219c1d
Teruyoshi Mitsuoka <TerryMitsuoka@gmail.com>
2013-06-28 12:25:24 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+https_servers
+==
+
+**https_servers** is a Dart sample HTTPS server applications.
+
+This is a Dart code sample and an attachment to the ["Dart Language Gide"](http://www.cresc.co.jp/tech/java/Google_Dart/DartLanguageGuide.pdf) written in Japanese.
+
+This repository consists of following source codes.
+
+- **https\_test\_server\_1.dart** : Simple HTTPS test server.
+
+- **https\_test\_server\_2.dart** : Simple HTTPS server. Can be used as a template for HTTP servers.
+
+このサンプルは[「プログラミング言語Dartの基礎」](http://www.cresc.co.jp/tech/java/Google_Dart/DartLanguageGuide_about.html)の
+添付資料です。詳細は「HTTPSサーバ (HTTPS Servers)」の章をご覧ください。
+
+### Installing ###
+
+1. Download this repository, uncompress and rename the folder to **https_servers**.
+2. From Dart Editor, File > Open Existing Folder and select this https_servers folder.
+
+### Try it ###
+
+1. Run **bin/https\_test\_server\_1.dart** or **bin/https\_test\_server\_2.dart** as server.
+2. Access these servers from your browser as `https://localhost/test`.
+
+
+### Notes ###
+
+  1. Attached NSS database includes self-signed certificate. Your browser will complain about certificate is not valid (but can easily bypass it by telling browser that you trust this site). If you have NSS database that includes certificate from trusted CA, replace **cert9.db** and **key4.db** with yours and change the **CER\_NICKNAME** and **DB\_PWD** values.
+
+  2. Other than on Windows, you may need to change the **DB\_DIR** value.
+
+### License ###
+This sample is licensed under [MIT License][MIT].
+[MIT]: http://www.opensource.org/licenses/mit-license.php
---
+++ bin/https_test_server_1.dart
@@ -0,0 +1,82 @@
+/*
+  Dart code sample: Simple HTTPS test server
+  1. Run the https_test_1.dart as server.
+  2. Access the server from Chrome: https://localhost/test
+  Typical response of the server will be:
+    This is a response from https_test_server_1.
+      req.connectionInfo.remoteHost : ::28ed:9707:0:0
+      req.connectionInfo.remotePort : 51045
+      req.uri : /test
+  Notes:
+  1. Attached NSS DB includes self-signed certificate. Your browser will complain
+     about certificate is not valid (but can easily bypass it by telling browser
+     that you trust this site). If you have NSS DB that includes certificate from
+     trusted CA, replace cert9.db and key4.db with yours and change CER_NICKNAME
+     and DB_PWD values.
+  2. Other than on Windows, you may need to change the DB_DIR value.
+  June 2013, by Terry Mitsuoka
+*/
+
+import 'dart:async';
+import 'dart:io';
+
+final HOST_NAME = 'localhost';   // use loop back address for the test
+final int SERVER_PORT = 443;     // use well known HTTPS port number
+final REQ_PATH = '/test';        // request path for this application
+final CER_NICKNAME = 'myissuer'; // nickname of the certificate
+final DB_PWD = 'changeit';       // NSS DB access pass word
+final DB_DIR = r'..\nss';        // NSS DB directory path
+final LOG_REQUESTS = true;
+
+void main() {
+  initializeSecureSocket();
+  listenHttpsRequest();
+}
+
+void initializeSecureSocket() {
+  SecureSocket.initialize(database: DB_DIR,
+                          password: DB_PWD,
+                          useBuiltinRoots: false);
+  log('NSS library initialized.');
+}
+
+void listenHttpsRequest() {
+  HttpServer.bindSecure(HOST_NAME,
+                        SERVER_PORT,
+                        certificateName: CER_NICKNAME)
+  .then((HttpServer server) {
+    server.listen(
+      (HttpRequest req) {
+        if (req.uri.path.contains(REQ_PATH)) processRequest(req);
+      },
+      onError: (err) {
+        print('listen: error: $err');
+      },
+      onDone: () {
+        print('listen: done');
+      },
+      cancelOnError: false
+      );
+    log('https_test_1 server started.');
+  });
+}
+
+void processRequest(HttpRequest req) {
+  String mes = requestInf(req);
+  if (LOG_REQUESTS) log('\n$mes');
+  req.response.write(
+    '''<!DOCTYPE html>This is a response from https_test_server_1.
+    <pre>$mes</pre></html>''');
+  req.response.close();
+}
+
+// adapt this function to your logger
+void log(String s) {
+  print('${new DateTime.now().toString()} - $s');
+}
+
+String requestInf(HttpRequest req) =>
+  '''
+  req.connectionInfo.remoteHost : ${req.connectionInfo.remoteHost}
+  req.connectionInfo.remotePort : ${req.connectionInfo.remotePort}
+  req.uri : ${req.uri}''';
